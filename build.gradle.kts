27a854c80578db21e8c61861d6db95787b3fe70a
mango <mchead1@naver.com>
2025-02-28 01:03:30 +0900

    chore: applied version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,35 +24,15 @@ extra["netflixDgsVersion"] = "10.0.3"
 extra["sentryVersion"] = "8.2.0"
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
-    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-    implementation("org.springframework.boot:spring-boot-starter-jooq")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter")
-    implementation("io.micrometer:micrometer-tracing-bridge-brave")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("io.sentry:sentry-spring-boot-starter-jakarta")
-    implementation("org.flywaydb:flyway-core")
-    implementation("org.flywaydb:flyway-database-postgresql")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("org.springframework:spring-jdbc")
-    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
-    runtimeOnly("org.postgresql:postgresql")
-    runtimeOnly("org.postgresql:r2dbc-postgresql")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.boot:spring-boot-testcontainers")
-    testImplementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter-test")
-    testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
-    testImplementation("org.testcontainers:elasticsearch")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:postgresql")
-    testImplementation("org.testcontainers:r2dbc")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(libs.bundles.languages)
+    implementation(libs.spring.starter.webflux)
+    implementation(libs.bundles.monitoring)
+    implementation(libs.bundles.db.setup)
+    runtimeOnly(libs.bundles.postgres)
+    implementation(libs.dgs.spring.graphql.starter)
+    testImplementation(libs.bundles.test.setup)
+    testImplementation(libs.bundles.test.db)
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
 
 dependencyManagement {
@@ -69,7 +49,7 @@ kotlin {
 }
 
 tasks.generateJava {
-    schemaPaths.add("${projectDir}/src/main/resources/graphql-client")
+    schemaPaths.add("$projectDir/src/main/resources/graphql-client")
     packageName = "io.contents.codegen"
     generateClient = true
 }
