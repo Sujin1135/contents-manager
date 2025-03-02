c4af2084a36345ef96dfd66936b72ec2c4769641
mango <mchead1@naver.com>
2025-03-03 00:49:59 +0900

    chore: set up test configuration

--- subproject/application/src/test/kotlin/TestcontainersConfiguration.kt
+++ subproject/application/src/test/kotlin/TestcontainersConfiguration.kt
@@ -4,15 +4,15 @@ import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
 import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.elasticsearch.ElasticsearchContainer
 import org.testcontainers.utility.DockerImageName
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
-    @Bean
-    @ServiceConnection
-    fun elasticsearchContainer(): ElasticsearchContainer =
-        ElasticsearchContainer(DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch:7.17.10"))
+//    TODO: configure
+//    @Bean
+//    @ServiceConnection
+//    fun elasticsearchContainer(): ElasticsearchContainer =
+//        ElasticsearchContainer(DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch:7.17.10"))
 
     @Bean
     @ServiceConnection
---
+++ subproject/application/src/test/kotlin/io/contents/collector/application/FindYoutubeHandlesTests.kt
@@ -0,0 +1,19 @@
+package io.contents.collector.io.contents.collector.application
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FindYoutubeHandlesTests :
+    BehaviorSpec({
+        context("it should be able to find Youtube handles by some keyword") {
+            given("a keyword") {
+                `when`("request to get youtube handles") {
+                    then("should return youtube handles related the given keyword") {
+                        1 shouldBe 1
+                    }
+                }
+            }
+        }
+    })
