886ad0102f4a6c8521157b32cadea73d4664094e
mango <mchead1@naver.com>
2026-02-08 22:52:02 +0900

    test: mangonaut 에러 트래킹 후 PR 생성 테스트를 위한 배포

--- subproject/boot/src/main/kotlin/io/contents/collector/SentryTestRunner.kt
+++ subproject/boot/src/main/kotlin/io/contents/collector/SentryTestRunner.kt
@@ -1,5 +1,6 @@
 package io.contents.collector
 
+import io.contents.collector.repository.ChannelRepositoryImpl
 import io.sentry.Sentry
 import org.slf4j.LoggerFactory
 import org.springframework.boot.context.event.ApplicationReadyEvent
@@ -8,16 +9,26 @@ import org.springframework.stereotype.Component
 
 @Component
 class SentryTestRunner {
-
     private val log = LoggerFactory.getLogger(this::class.java)
 
     @EventListener(ApplicationReadyEvent::class)
     fun sendTestEvent() {
         try {
-            throw RuntimeException("Sentry test error - collector application started")
-        } catch (e: RuntimeException) {
+            simulateNullPointerInToDomain()
+        } catch (e: Exception) {
             Sentry.captureException(e)
             log.info("Sentry test issue has been sent successfully")
         }
+    }
+
+    /**
+     * ChannelRepositoryImpl.toDomain()에서 DB 레코드의 nullable 필드에
+     * !! 연산자를 사용하여 발생할 수 있는 NullPointerException을 시뮬레이션
+     */
+    private fun simulateNullPointerInToDomain() {
+        val nullableDescription: String? = null
+        // ChannelRepositoryImpl.kt:74 의 this.description!! 와 동일한 패턴
+        ChannelRepositoryImpl::class.java // stack trace에 클래스 참조 포함
+        val description = nullableDescription!!
     }
 }
