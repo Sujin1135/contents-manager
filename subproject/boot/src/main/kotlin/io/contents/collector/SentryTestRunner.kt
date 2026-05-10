4c4975fe305f082dd75fd43c72c1b1e2125a405a
mango <mchead1@naver.com>
2026-05-10 23:36:22 +0900

    test(mangonaut): for mangonaut test

--- subproject/boot/src/main/kotlin/io/contents/collector/SentryTestRunner.kt
+++ subproject/boot/src/main/kotlin/io/contents/collector/SentryTestRunner.kt
@@ -14,7 +14,7 @@ class SentryTestRunner {
     @EventListener(ApplicationReadyEvent::class)
     fun sendTestEvent() {
         try {
-            simulateNullPointerInToDomain()
+            simulateNullPointerInToDomainV2()
         } catch (e: Exception) {
             Sentry.captureException(e)
             log.info("Sentry test issue has been sent successfully")
@@ -25,7 +25,7 @@ class SentryTestRunner {
      * ChannelRepositoryImpl.toDomain()에서 DB 레코드의 nullable 필드에
      * !! 연산자를 사용하여 발생할 수 있는 NullPointerException을 시뮬레이션
      */
-    private fun simulateNullPointerInToDomain() {
+    private fun simulateNullPointerInToDomainV2() {
         val nullableDescription: String? = null
         // ChannelRepositoryImpl.kt:74 의 this.description!! 와 동일한 패턴
         ChannelRepositoryImpl::class.java // stack trace에 클래스 참조 포함
